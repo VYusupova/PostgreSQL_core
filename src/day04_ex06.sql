a8f5da5c66d72afe9270774dabfc3f1ece2da363
VYusupova <vopengame@gmail.com>
2024-12-15 23:21:46 +0300

    day_04_3

---
+++ src/day04_ex06.sql
@@ -0,0 +1,8 @@
+CREATE MATERIALIZED VIEW mv_dmitriy_visits_and_eats AS
+SELECT DISTINCT pizzeria.name from person_visits
+JOIN person ON person.id = person_id
+JOIN pizzeria ON pizzeria.id = pizzeria_id
+JOIN menu ON menu.pizzeria_id = pizzeria.id
+WHERE person.name = 'Dmitriy'
+AND visit_date = '2022-01-08'
+and price < 800
