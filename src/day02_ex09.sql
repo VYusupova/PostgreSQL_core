957c7ba99af04582271e71f5a0ffa366586d0b38
veronika <vopengame@gmail.com>
2024-12-20 17:43:55 +0300

    day02_ex09_уточнение задания

--- src/day02_ex09.sql
+++ src/day02_ex09.sql
@@ -1,14 +1,19 @@
 
 WITH pop (id) AS
 (
-SELECT person_id as id 
+ SELECT person_id
   FROM person_order
   JOIN menu ON menu_id = menu.id
- WHERE pizza_name in ( 'pepperoni pizza', 'cheese pizza')
+ WHERE pizza_name in ( 'cheese pizza')
+ INTERSECT
+ SELECT person_id
+  FROM person_order
+  JOIN menu ON menu_id = menu.id
+ WHERE pizza_name in ( 'pepperoni pizza')
 )
- 
+
  SELECT DISTINCT person.name
    FROM person
-   join pop ON person.id = pop.id 
-   WHERE gender = 'female' 
- ORDER BY 1 
+   JOIN pop ON person.id = pop.id
+   WHERE gender = 'female'
+ ORDER BY 1
