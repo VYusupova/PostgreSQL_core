6c8473240495b5e48955d8d2da0db2b98aaa7f65
veronika <vopengame@gmail.com>
2024-12-20 17:47:00 +0300

    day02_ex10_уточнениние в названии колонки

--- src/day02_ex10.sql
+++ src/day02_ex10.sql
@@ -1,10 +1,7 @@
-SELECT
-	person1.name AS person_name1, person2.name AS person_name2, person1.address
-FROM
-	person AS person1
-JOIN
-	person AS person2 ON person1.address = person2.address
-WHERE
-	person1.id > person2.id
-ORDER BY
-	1, 2, 3;
+SELECT	person1.name AS person_name1, 
+        person2.name AS person_name2, 
+        person1.address AS common_address
+   FROM person AS person1
+   JOIN person AS person2 ON person1.address = person2.address
+  WHERE person1.id > person2.id
+ORDER BY 1, 2, 3;
