cc49ab91dbcb89c923a6bd876bb5e2288b5c8e25
veronika <vopengame@gmail.com>
2024-12-22 13:21:58 +0300

    day03_ex06_nice

--- src/day03_ex06.sql
+++ src/day03_ex06.sql
@@ -1,11 +1,20 @@
-
- With pizzeria1 AS
+WITH pizzeria AS
  (
-  select menu.id, pizza_name, pizzeria.name, price FROM menu 
-   JOIN pizzeria ON menu.pizzeria_id = pizzeria.id
+  SELECT menu.id,
+         pizza_name,
+         pizzeria.name AS pizzeria_name, 
+         price
+    FROM menu
+    JOIN pizzeria ON menu.pizzeria_id = pizzeria.id
  )
- 			
- SELECT p1.pizza_name, p1.name AS pizzeria_name_1, p2.name AS pizzeria_name_2, p1.price FROM pizzeria1 p1
- JOIN pizzeria1 p2 ON p1.pizza_name = p2.pizza_name AND p1.name != p2.name AND p1.price = p2.price
+
+ SELECT p1.pizza_name,
+        p1.pizzeria_name AS pizzeria_name_1,
+        p2.pizzeria_name AS pizzeria_name_2,
+        p1.price
+   FROM pizzeria p1
+   JOIN pizzeria p2 ON p1.pizza_name = p2.pizza_name
+                     AND p1.pizzeria_name != p2.pizzeria_name
+                     AND p1.price = p2.price
  WHERE p1.id > p2.id
  ORDER BY p1.pizza_name
