b763cfd65559e25a16beca1a37ece97e2c95282d
veronika <vopengame@gmail.com>
2024-12-22 13:14:47 +0300

    day03_ex05_+_nice

--- src/day03_ex05.sql
+++ src/day03_ex05.sql
@@ -1,17 +1,21 @@
-WITH visit AS 
-( Select pizzeria_id, person.name
-    from person_visits
+WITH 
+visit AS (
+  SELECT pizzeria_id, person.name
+    FROM person_visits
     JOIN person ON person_id = person.id 
 ),
 orders AS (
-Select pizzeria_id, person.name from person_order 
-JOIN person ON person_id = person.id
-JOIN menu ON menu_id = menu.id
+  SELECT pizzeria_id, person.name 
+    FROM person_order 
+    JOIN person ON person_id = person.id
+    JOIN menu ON menu_id = menu.id
 )
 
-SELECT name as pizzeria_name FROM pizzeria
-JOIN(
-Select pizzeria_id FROM visit where name = 'Andrey'
-EXCEPT
-SELECT pizzeria_id FROM orders where name = 'Andrey'
+SELECT name AS pizzeria_name 
+  FROM pizzeria
+  JOIN(
+       SELECT pizzeria_id FROM visit WHERE name = 'Andrey'
+       EXCEPT
+       SELECT pizzeria_id FROM orders WHERE name = 'Andrey'
 ) ON pizzeria.id = pizzeria_id
+ORDER BY 1
