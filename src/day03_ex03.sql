5194937c4f3232d3eb805998489a8cadb41d6cf1
veronika <vopengame@gmail.com>
2024-12-22 12:59:15 +0300

    day03_ex03_nice

--- src/day03_ex03.sql
+++ src/day03_ex03.sql
@@ -1,28 +1,28 @@
 WITH 
 female_visit AS (
 	SELECT pizzeria.name AS pizzeria_name, 
-           Count(pizzeria_id) as cnt
-  	  from pizzeria
+           Count(pizzeria_id) AS cnt
+  	  FROM pizzeria
       JOIN person_visits ON pizzeria.id = person_visits.pizzeria_id
-      Join person        ON person_id = person.id and person.gender = 'female' 
+      JOIN person        ON person_id = person.id AND person.gender = 'female' 
   GROUP BY 1
   ),
 male_visit AS (
     SELECT pizzeria.name AS pizzeria_name , 
-           Count(pizzeria_id) as cnt 
-  	  from pizzeria
+           Count(pizzeria_id) AS cnt 
+  	  FROM pizzeria
       JOIN person_visits ON pizzeria.id = person_visits.pizzeria_id
-      Join person        ON person_id = person.id and person.gender = 'male'
+      JOIN person        ON person_id = person.id AND person.gender = 'male'
   GROUP BY 1
   )
 
 SELECT female_visit.pizzeria_name
-  From female_visit 
+  FROM female_visit 
   JOIN male_visit ON female_visit.pizzeria_name = male_visit.pizzeria_name 
-   and female_visit.cnt > male_visit.cnt
+WHERE  female_visit.cnt > male_visit.cnt
 UNION ALL
 SELECT female_visit.pizzeria_name
-  From female_visit
+  FROM female_visit
   JOIN male_visit ON female_visit.pizzeria_name = male_visit.pizzeria_name 
-   and female_visit.cnt < male_visit.cnt
+WHERE female_visit.cnt < male_visit.cnt
 ORDER BY 1
