2fb1e8b0faee4b5a585aa051325a6ee754bfe7aa
veronika <vopengame@gmail.com>
2024-12-22 12:40:45 +0300

    day03_ex01_nice

--- src/day03_ex01.sql
+++ src/day03_ex01.sql
@@ -1,1 +1,5 @@
-SELECT id FROM menu WHERE id NOT IN (SELECT menu_id FROM person_order) ORDER BY id
+SELECT id 
+  FROM menu 
+ WHERE id NOT IN (SELECT menu_id 
+                    FROM person_order) 
+ORDER BY id
