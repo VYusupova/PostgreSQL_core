cfbedbbf81ae1351f81ea7dccb1690e3677256e0
veronika <vopengame@gmail.com>
2025-01-03 21:09:09 +0300

    Update team00_ex01.sql

--- src/team00_ex01.sql
+++ src/team00_ex01.sql
@@ -1,4 +1,7 @@
 SELECT total_cost, 
        tour
   FROM tour_all_cities 
-ORDER BY total_cost, tour; 
+  WHERE strpos(tour,'A') = 2
+  AND (total_cost = (SELECT MAX(total_cost) FROM tour_all_cities)
+    OR total_cost = (SELECT MIN(total_cost) FROM tour_all_cities))
+  ORDER BY total_cost, tour;  
