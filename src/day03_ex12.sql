8b1db722adbd99d82633b2a5aa80086356980ed8
veronika <vopengame@gmail.com>
2025-01-05 17:42:40 +0300

    INTO person_order

--- src/day03_ex12.sql
+++ src/day03_ex12.sql
@@ -4,6 +4,6 @@ INSERT INTO person_order ( ID, person_id, menu_id, order_date)
 SELECT (SELECT MAX(id) FROM person_order)+id,
        id,
        (SELECT id FROM menu WHERE pizza_name = 'greek pizza'),
-       '2022-02-25' as order_date 
+       '2022-02-25' AS order_date 
    FROM person
 
