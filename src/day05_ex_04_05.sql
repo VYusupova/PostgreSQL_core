8089acb0d8e728121daecbba5fc252ee712e71b9
veronika <vopengame@gmail.com>
2025-01-05 19:59:28 +0300

    UNIQUE INDEX + WHERE

--- src/day05_ex_04_05.sql
+++ src/day05_ex_04_05.sql
@@ -26,3 +26,27 @@ INSERT INTO
 VALUES
       (21, 4, 'mushroom pizza', 1000);
       
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+|  Exercise 05 - Partial uniqueness for data (Частичная уникальность данных)   |
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- ---- -- -- -- -- -- -- -- 
+
+/* idx_person_order_order_date
+ в таблице person_order для атрибутов person_id и menu_id 
+ с частичной уникальностью для столбца order_date для даты `2022-01-
+*/
+CREATE UNIQUE INDEX if not EXISTS idx_person_order_order_date
+	ON  person_order (person_id, menu_id)
+    WHERE order_date = '2022-01-01';
+
+--DROP INDEX idx_person_order_order_date;
+
+SET ENABLE_SEQSCAN TO OFF;
+
+EXPLAIN ANALYZE 
+SELECT DISTINCT person_id, menu_id 
+FROM person_order WHERE  order_date = '2022-01-01';
+
+
+SET ENABLE_SEQSCAN TO ON;
+
+-- Index Only Scan using idx_person_order_order_date on person_order …
