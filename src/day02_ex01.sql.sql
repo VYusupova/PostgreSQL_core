3f8ba04d44a0f287d42d4257a58d467c410b1a23
veronika <vopengame@gmail.com>
2025-01-06 18:49:44 +0300

    Delete day02_ex01.sql

--- src/day02_ex01.sql.sql
+++
@@ -1,8 +0,0 @@
-    SELECT DISTINCT pv2.visit_date AS missing_date
-      FROM (SELECT visit_date
-           FROM  person_visits 
-           WHERE person_id BETWEEN 1 AND 2
-           ) AS pv1
-RIGHT JOIN person_visits pv2 ON pv1.visit_date = pv2.visit_date
-     WHERE pv1.visit_date IS NULL
-  ORDER BY pv2.visit_date ASC
