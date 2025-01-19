e1513a4c891047ae15cd54322a2b9a76ba0fb9e6
veronika <vopengame@gmail.com>
2025-01-19 17:52:02 +0300

    Delete src/day02_ex04.sql

--- src/day02_ex04.sql
+++
@@ -1,7 +0,0 @@
-WITH pizza (pizzeria_id, pizza_name, price) AS (
-SELECT pizzeria_id, pizza_name, price from menu where pizza_name in ('mushroom pizza', 'pepperoni pizza')
- )
- 
- SELECT pizza_name, NAME as pizzeria_name, price from pizza
- JOIN pizzeria ON id = pizzeria_id
- ORDER BY 1,2
