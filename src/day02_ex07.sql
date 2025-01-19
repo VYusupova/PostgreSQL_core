812fabecfb58b8938c015f6ce26653fb255015a8
veronika <vopengame@gmail.com>
2025-01-19 17:51:02 +0300

    Delete src/day02_ex07.sql

--- src/day02_ex07.sql
+++
@@ -1,8 +0,0 @@
-
-SELECT pizzeria.name FROM person 
-JOIN person_visits ON person_id = person.id
-JOIN pizzeria      ON pizzeria.id = pizzeria_id
-JOIN menu		   ON menu.pizzeria_id = pizzeria.id	
-where person.NAME = 'Dmitriy'
-and visit_date = '2022-01-08'
-AND price < 800
