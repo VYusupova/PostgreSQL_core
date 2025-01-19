33ded1898ca1dc88305e20ff44325cb6dba3b6c0
veronika <vopengame@gmail.com>
2025-01-19 11:22:46 +0300

    INTERSECT  и подзапросы в селекте

--- src/day01_ex06_v1.sql
+++
@@ -1,4 +0,0 @@
-SELECT order_date as action_date, (Select name From person where id = person_id) as person_name from person_order 
-INTERSECT
-select visit_date as action_date, (Select name From person where id = person_id) as person_name from person_visits
-order by 1 ASC, 2 DESC
