e4e0a43701c9be238f72400cdcaef19eccd94f1e
veronika <vopengame@gmail.com>
2025-01-20 16:13:46 +0300

    Delete src/day04_ex06.sql

--- src/day04_ex06.sql
+++
@@ -1,9 +0,0 @@
-CREATE MATERIALIZED VIEW mv_dmitriy_visits_and_eats AS
-SELECT DISTINCT pizzeria.name 
-  FROM person_visits
-  JOIN person   ON person.id = person_id
-  JOIN pizzeria ON pizzeria.id = pizzeria_id
-  JOIN menu     ON menu.pizzeria_id = pizzeria.id
- WHERE person.name = 'Dmitriy'
-   AND visit_date = '2022-01-08'
-   AND price < 800
