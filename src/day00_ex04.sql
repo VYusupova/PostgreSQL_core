9d628550a7931e0beb17fbba679ebf52e9981bba
veronika <vopengame@gmail.com>
2025-02-02 14:11:53 +0300

    DEL day00_ex04.sql

--- src/day00_ex04.sql
+++
@@ -1,8 +0,0 @@
---`Anna (age:16,gender:'female',address:'Moscow')`
-
-SELECT CONCAT(name, ' (', 
-              'age:', age , 
-              ',gender:''', gender, 
-              ''',address:''', address, 
-              ''')') as person_information
-  FROM person
