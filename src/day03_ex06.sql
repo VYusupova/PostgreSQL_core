b4c81972aaea92b36fabf2a9a8fac7c6bc2825e6
veronika <vopengame@gmail.com>
2025-02-04 18:40:37 +0300

    небольшие недочеты.sql

--- src/day03_ex06.sql
+++ src/day03_ex06.sql
@@ -18,3 +18,27 @@ WITH pizzeria AS
                      AND p1.price = p2.price
  WHERE p1.id > p2.id
  ORDER BY p1.pizza_name
+--my output -----------------------------------------------------
+-- _____________________________________________________________
+-- cheese pizza	      |  Best Pizza  |  	Papa Johns	  |  700
+-- pepperoni pizza	   |  Best Pizza  |  	DinoPizza	   |  800
+-- supreme pizza	     |  Best Pizza  |  	DoDo Pizza	  |  850
+-- _____________________________________________________________
+
+-- original --- 
+SELECT m1.pizza_name, 
+       p1.name AS pizzeria_name_1, 
+       p2.name AS pizzeria_name_2, 
+       m1.price 
+FROM menu m1 
+INNER JOIN menu m2 ON m1.id <> m2.id AND m1.price = m2.price AND m1.pizzeria_id > m2.pizzeria_id 
+AND m1.pizza_name = m2.pizza_name 
+INNER JOIN pizzeria p1 ON m1.pizzeria_id = p1.id
+INNER JOIN pizzeria p2 ON m2.pizzeria_id = p2.id 
+ORDER BY 1
+
+-- _____________________________________________________________
+-- cheese pizza	      |  Best Pizza  |  	Papa Johns	  |  700
+-- pepperoni pizza	   | 	DinoPizza	  |   Best Pizza   |  800  <<-- this String 
+-- supreme pizza	     |  Best Pizza  |  	DoDo Pizza	  |  850
+-- _____________________________________________________________
