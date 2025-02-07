24eb708ffe31638ce465f0f5d4ef15517c576b7d
veronika <vopengame@gmail.com>
2025-02-07 16:22:52 +0300

    Delete src/day00_ex05.sql

--- src/day00_ex05.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-       ( SELECT name 
-           From person 
-          where id = person_id 
-       ) as name
-  from person_order 
- where menu_id in (13,14,18)
-   and order_date = '2022-01-07';
