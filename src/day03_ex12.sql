a53cb3705fea3eb19a9ced83e6b30845a02b19af
veronika <vopengame@gmail.com>
2025-02-07 17:53:54 +0300

    INSERT DELETE

--- src/day03_ex12.sql
+++ src/day03_ex12.sql
@@ -6,4 +6,13 @@ SELECT (SELECT MAX(id) FROM person_order)+id,
        (SELECT id FROM menu WHERE pizza_name = 'greek pizza'),
        '2022-02-25' AS order_date 
    FROM person
+;
+
+-- Упражнение 13 ----------------------------------------------------------------
+
+DELETE FROM  person_order
+       WHERE menu_id =(SELECT id FROM menu WHERE pizza_name = 'greek pizza')
+         and order_date = '2022-02-25' ж
+ 
+DELETE FROM menu WHERE pizza_name = 'greek pizza'
 
