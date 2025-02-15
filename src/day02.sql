f358615c552e6932099c37dc5c3cbde282c69507
veronika <vopengame@gmail.com>
2025-02-15 13:58:41 +0300

    WITH

--- src/day02.sql
+++ src/day02.sql
@@ -1,9 +1,13 @@
-   SELECT NAME, 
+-- ex00 -- необходимо вернуть список названий пиццерий с рейтингом которые никто не посещал
+SELECT NAME, 
           rating 
      FROM pizzeria 
 LEFT JOIN person_visits ON pizzeria.id = pizzeria_id
 WHERE pizzeria_id is NULL;
 
+-- ex01 -- вернуть потерянные даты с 1 по 10 января 2022 (включительно) 
+-----------посещений с идентификатором 1 или 2 (это значит дней дней утеряно оба)
+-----------отсортируйте дни посещений по возрастанию
     SELECT DISTINCT pv2.visit_date AS missing_date
       FROM (SELECT visit_date
            FROM  person_visits 
@@ -13,6 +17,10 @@ RIGHT JOIN person_visits pv2 ON pv1.visit_date = pv2.visit_date
      WHERE pv1.visit_date IS NULL
   ORDER BY pv2.visit_date ASC;
 
+-- ex02 -- вернуть весь список имен посетителей( или не посетителей)пиццерий 
+-----------за период с 1 по 3 января 2022 с одной стороны и весь список названий пиццерий 
+-----------который имеет и не имет посетителй с другой стороны. 
+--------------Обратите внимание на замену ‘-’ for NULL значений в столбцах.
     SELECT  CASE WHEN (person.name IS NULL) 
                  THEN '-'
                  ELSE person.name 
@@ -31,3 +39,99 @@ RIGHT JOIN person_visits pv2 ON pv1.visit_date = pv2.visit_date
     FULL JOIN pizzeria ON pizzeria.id = pv1.pizzeria_id
     FULL JOIN person   ON person.id = pv1.person_id
     ORDER BY 1,2,3
+-- ex03 -- вернемся к упраженению 1 перепешите ваш SQL с использованием СТЕ паттерна. перемести в часть СТЕ ваш "генератор дней". Вывод должен быть таким же как в упражнениии 1
+
+   WITH day_generator (visit_date) AS 
+   (SELECT visit_date
+           FROM  person_visits 
+           WHERE person_id BETWEEN 1 AND 2
+   )
+   
+   SELECT DISTINCT pv2.visit_date AS missing_date
+      FROM day_generator
+RIGHT JOIN person_visits pv2 ON day_generator.visit_date = pv2.visit_date
+     WHERE day_generator.visit_date IS NULL
+  ORDER BY pv2.visit_date
+
+-- ex04 --__________________________
+WITH pizza (pizzeria_id, pizza_name, price) AS (
+SELECT pizzeria_id, pizza_name, price from menu where pizza_name in ('mushroom pizza', 'pepperoni pizza')
+ )
+ 
+ SELECT pizza_name, NAME as pizzeria_name, price from pizza
+ JOIN pizzeria ON id = pizzeria_id
+ ORDER BY 1,2
+
+---- ex05 -- __________________________
+
+SELECT name
+  FROM person
+ WHERE age > 25
+   AND gender = 'female'
+ORDER BY name
+--- ex06 -- __________________________
+
+  SELECT menu.pizza_name, 
+         pizzeria.name 
+    FROM person 
+    JOIN person_order ON person.id = person_id 
+    JOIN menu         ON menu_id = menu.id
+    JOIN pizzeria     ON pizzeria_id = pizzeria.id
+   WHERE person.name IN ('Anna', 'Denis') 
+ ORDER BY 1,2
+
+---- ex07 -- __________________________
+
+SELECT pizzeria.name FROM person 
+JOIN person_visits ON person_id = person.id
+JOIN pizzeria      ON pizzeria.id = pizzeria_id
+JOIN menu		   ON menu.pizzeria_id = pizzeria.id	
+where person.NAME = 'Dmitriy'
+and visit_date = '2022-01-08'
+AND price < 800
+
+---- ex08 -- __________________________
+WITH pop (id) AS
+(
+SELECT person_id as id 
+  FROM person_order
+  JOIN menu ON menu_id = menu.id
+ WHERE pizza_name in ( 'pepperoni pizza', 'mushroom pizza')
+)
+ 
+ SELECT person.name 
+   FROM person
+   join pop ON person.id = pop.id 
+  WHERE gender = 'male' 
+    AND address in ('Moscow', 'Samara')
+ ORDER BY 1 DESC
+
+---- ex09 -- __________________________
+WITH pop (id) AS
+(
+ SELECT person_id
+  FROM person_order
+  JOIN menu ON menu_id = menu.id
+ WHERE pizza_name in ( 'cheese pizza')
+ INTERSECT
+ SELECT person_id
+  FROM person_order
+  JOIN menu ON menu_id = menu.id
+ WHERE pizza_name in ( 'pepperoni pizza')
+)
+
+       
+---- ex10 -- __________________________
+ SELECT DISTINCT person.name
+   FROM person
+   JOIN pop ON person.id = pop.id
+   WHERE gender = 'female'
+ ORDER BY 1
+
+SELECT	person1.name AS person_name1, 
+        person2.name AS person_name2, 
+        person1.address AS common_address
+   FROM person AS person1
+   JOIN person AS person2 ON person1.address = person2.address
+  WHERE person1.id > person2.id
+ORDER BY 1, 2, 3;
