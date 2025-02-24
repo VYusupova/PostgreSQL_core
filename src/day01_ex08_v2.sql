3245725dd41f098d4e02b5a13c07dc1d532d29ef
veronika <vopengame@gmail.com>
2025-02-24 14:13:19 +0300

    Delete src/day01_ex08_v2.sql

--- src/day01_ex08_v2.sql
+++
@@ -1,9 +0,0 @@
---Andrey (age:21) 
-
-       select order_date,
-              person_information 
-         from person_order
- NATURAL JOIN (SELECT id AS person_id,
-                      CONCAT(person.NAME, ' (age:', person.age, ')') as  person_information 
-                 FROM person)
- ORDER BY 1,2
