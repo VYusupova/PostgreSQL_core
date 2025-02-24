59417f85a5753fc0e6c834824001b3fb8deaa439
veronika <vopengame@gmail.com>
2025-02-24 14:14:41 +0300

    JOIN & NATURAL JOIN

--- src/day01_ex07.sql
+++ src/day01_ex07.sql
@@ -4,3 +4,25 @@ select order_date,
        CONCAT(person.NAME, ' (age:', person.age, ')') as  person_information from person_order
  JOIN person  ON person.id = person_id
  ORDER BY 1,2
+
+--EX08 -- 2 version
+       --#version 1
+--Andrey (age:21) 
+
+       select order_date,
+              CONCAT(person.NAME, ' (age:', person.age, ')') as  person_information 
+         from (SELECT order_date, 
+                      person_id as id 
+                 from person_order)
+ NATURAL JOIN person 
+ ORDER BY 1,2
+   --#version 2
+--Andrey (age:21) 
+
+       select order_date,
+              person_information 
+         from person_order
+ NATURAL JOIN (SELECT id AS person_id,
+                      CONCAT(person.NAME, ' (age:', person.age, ')') as  person_information 
+                 FROM person)
+ ORDER BY 1,2
