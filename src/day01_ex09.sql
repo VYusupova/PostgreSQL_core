ca6266b83563c5855bc1e12d9ba6471af3a8ab27
veronika <vopengame@gmail.com>
2025-02-27 13:58:16 +0300

    Delete src/day01_ex09.sql

--- src/day01_ex09.sql
+++
@@ -1,4 +0,0 @@
-
-Select name from pizzeria where id not in (SELECT pizzeria_id from person_visits)
-
-Select name from pizzeria where not EXISTS (SELECT pizzeria_id from person_visits where pizzeria.id = pizzeria_id)
