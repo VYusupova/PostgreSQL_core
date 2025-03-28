2fd1111650a67a37c37cfcf0ac91a2cb6e3546c2
veronika <vopengame@gmail.com>
2025-03-28 16:09:12 +0300

    FULL RIGHT LEFT JOIN

--- src/day02.sql
+++ src/day02.sql
@@ -1,11 +1,11 @@
--- ex00 -- необходимо вернуть список названий пиццерий с рейтингом которые никто не посещал
+----------------  (Упражение) 00  -- необходимо вернуть список названий пиццерий с рейтингом которые никто не посещал
 SELECT NAME, 
           rating 
      FROM pizzeria 
 LEFT JOIN person_visits ON pizzeria.id = pizzeria_id
 WHERE pizzeria_id is NULL;
 
--- ex01 -- вернуть потерянные даты с 1 по 10 января 2022 (включительно) 
+----------------  (Упражение) 01 -- вернуть потерянные даты с 1 по 10 января 2022 (включительно) 
 -----------посещений с идентификатором 1 или 2 (это значит дней дней утеряно оба)
 -----------отсортируйте дни посещений по возрастанию
     SELECT DISTINCT pv2.visit_date AS missing_date
@@ -17,7 +17,7 @@ RIGHT JOIN person_visits pv2 ON pv1.visit_date = pv2.visit_date
      WHERE pv1.visit_date IS NULL
   ORDER BY pv2.visit_date ASC;
 
--- ex02 -- вернуть весь список имен посетителей( или не посетителей)пиццерий 
+----------------  (Упражение)02 -- вернуть весь список имен посетителей( или не посетителей)пиццерий 
 -----------за период с 1 по 3 января 2022 с одной стороны и весь список названий пиццерий 
 -----------который имеет и не имет посетителй с другой стороны. 
 --------------Обратите внимание на замену ‘-’ for NULL значений в столбцах.
@@ -39,7 +39,7 @@ RIGHT JOIN person_visits pv2 ON pv1.visit_date = pv2.visit_date
     FULL JOIN pizzeria ON pizzeria.id = pv1.pizzeria_id
     FULL JOIN person   ON person.id = pv1.person_id
     ORDER BY 1,2,3
--- ex03 -- вернемся к упраженению 1 перепешите ваш SQL с использованием СТЕ паттерна. перемести в часть СТЕ ваш "генератор дней". Вывод должен быть таким же как в упражнениии 1
+----------------  (Упражение)03 -- вернемся к упраженению 1 перепешите ваш SQL с использованием СТЕ паттерна. перемести в часть СТЕ ваш "генератор дней". Вывод должен быть таким же как в упражнениии 1
 
    WITH day_generator (visit_date) AS 
    (SELECT visit_date
@@ -53,7 +53,7 @@ RIGHT JOIN person_visits pv2 ON day_generator.visit_date = pv2.visit_date
      WHERE day_generator.visit_date IS NULL
   ORDER BY pv2.visit_date
 
--- ex04 --__________________________
+----------------  (Упражение)04 --__________________________
 WITH pizza (pizzeria_id, pizza_name, price) AS (
 SELECT pizzeria_id, pizza_name, price from menu where pizza_name in ('mushroom pizza', 'pepperoni pizza')
  )
@@ -62,14 +62,14 @@ SELECT pizzeria_id, pizza_name, price from menu where pizza_name in ('mushroom p
  JOIN pizzeria ON id = pizzeria_id
  ORDER BY 1,2
 
----- ex05 -- __________________________
+----------------  (Упражение)05 -- __________________________
 
 SELECT name
   FROM person
  WHERE age > 25
    AND gender = 'female'
 ORDER BY name
---- ex06 -- __________________________
+----------------  (Упражение)06 -- __________________________
 
   SELECT menu.pizza_name, 
          pizzeria.name 
@@ -80,7 +80,7 @@ ORDER BY name
    WHERE person.name IN ('Anna', 'Denis') 
  ORDER BY 1,2
 
----- ex07 -- __________________________
+----------------  (Упражение)07 -- __________________________
 
 SELECT pizzeria.name FROM person 
 JOIN person_visits ON person_id = person.id
@@ -90,7 +90,7 @@ where person.NAME = 'Dmitriy'
 and visit_date = '2022-01-08'
 AND price < 800
 
----- ex08 -- __________________________
+----------------  (Упражение)08 -- __________________________
 WITH pop (id) AS
 (
 SELECT person_id as id 
@@ -106,7 +106,7 @@ SELECT person_id as id
     AND address in ('Moscow', 'Samara')
  ORDER BY 1 DESC
 
----- ex09 -- __________________________
+----------------  (Упражение)09 -- __________________________
 WITH pop (id) AS
 (
  SELECT person_id
@@ -120,15 +120,14 @@ WITH pop (id) AS
  WHERE pizza_name in ( 'pepperoni pizza')
 )
 
-       
----- ex10 -- __________________________
- SELECT DISTINCT person.name
+   SELECT DISTINCT person.name
    FROM person
    JOIN pop ON person.id = pop.id
    WHERE gender = 'female'
  ORDER BY 1
+----------------  (Упражение) 10 -- Найти все людей, которые живут по одному адресу. __________________________
 
-SELECT	person1.name AS person_name1, 
+SELECT  person1.name AS person_name1, 
         person2.name AS person_name2, 
         person1.address AS common_address
    FROM person AS person1
