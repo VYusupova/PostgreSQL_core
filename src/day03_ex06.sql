6aa91da71c674406c4c070b1c7de55da85b80003
veronika <vopengame@gmail.com>
2026-09-11 15:31:28 +0300

    INSERT

--- src/day03_ex06.sql
+++ src/day03_ex06.sql
@@ -1,3 +1,12 @@
+-- ex 06 Найти несколько пицц названия которых имеют одинаковую стоимость но в разных пицериях.
+-- -- Убедитесь что результат отсортирован по названию пицц. Простоой вывод представлен ниже. Пожалуйста проверьте что названия столбцов такие же как ниже.
+--my output -----------------------------------------------------
+-- _____________________________________________________________
+-- cheese pizza	      |  Best Pizza  |  	Papa Johns	  |  700
+-- pepperoni pizza	   |  Best Pizza  |  	DinoPizza	   |  800
+-- supreme pizza	     |  Best Pizza  |  	DoDo Pizza	  |  850
+-- _____________________________________________________________
+
 WITH pizzeria AS
  (
   SELECT menu.id,
@@ -18,12 +27,6 @@ WITH pizzeria AS
                      AND p1.price = p2.price
  WHERE p1.id > p2.id
  ORDER BY p1.pizza_name
---my output -----------------------------------------------------
--- _____________________________________________________________
--- cheese pizza	      |  Best Pizza  |  	Papa Johns	  |  700
--- pepperoni pizza	   |  Best Pizza  |  	DinoPizza	   |  800
--- supreme pizza	     |  Best Pizza  |  	DoDo Pizza	  |  850
--- _____________________________________________________________
 
 -- original --- 
 SELECT m1.pizza_name, 
@@ -42,3 +45,51 @@ ORDER BY 1
 -- pepperoni pizza	   | 	DinoPizza	  |   Best Pizza   |  800  <<-- this String 
 -- supreme pizza	     |  Best Pizza  |  	DoDo Pizza	  |  850
 -- _____________________________________________________________
+
+
+--EX 07 зарегистрировать новую пиццу “greek pizza” (use id = 19) ценой 800 руб в ресторане “Dominos” (pizzeria_id = 2).
+INSERT INTO menu (id, pizzeria_id, pizza_name, price)  VALUES (19, 2, 'greek pizza', 800)
+-- new variant 
+ -- declare @name_pizza varchar (100) = 'greek pizza'
+ -- declare @price int = 800
+ -- declare @pizzeriaid int = (SELECT pizzeria_id FROM pizzeria WHERE name = 'Dominos')
+ -- INSERT INTO menu (id, pizzeria_id, pizza_name, price)  VALUES (19, @pizzeriaid, @name_pizza, @price)
+--EX 08
+INSERT INTO menu (id, pizzeria_id, pizza_name, price)  
+VALUES ((SELECT MAX(id)+1 FROM menu),
+        (SELECT id FROM pizzeria WHERE name = 'Dominos'), 
+        'sicilian pizza', 
+        900)
+
+--EX 09
+INSERT INTo person_visits (id, person_id, pizzeria_id, visit_date)
+VALUES(
+  (SELECT MAX(id)+1 FROM person_visits),
+  (SELECT id FROM person WHERE name = 'Denis'),
+  (SELECT id FROM pizzeria WHERE name = 'Dominos'),
+  '2022-02-24'
+  ),
+  (
+  (SELECT MAX(id)+2 FROM person_visits),
+  (SELECT id FROM person WHERE name = 'Irina'),
+  (SELECT id FROM pizzeria WHERE name = 'Dominos'),
+  '2022-02-24'
+  )
+
+--EX 10
+
+INSERT INTo person_order (id, person_id, menu_id, order_date)
+VALUES(
+  (SELECT MAX(id)+1 FROM person_order),
+  (SELECT id FROM person WHERE name = 'Denis'),
+  (SELECT id FROM menu WHERE pizza_name = 'sicilian pizza'),
+  '2022-02-24'
+  ),
+  (
+  (SELECT MAX(id)+2 FROM person_order),
+  (SELECT id FROM person WHERE name = 'Irina'),
+  (SELECT id FROM menu WHERE pizza_name = 'sicilian pizza'),
+  '2022-02-24'
+  )
+
+   
